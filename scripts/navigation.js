62e3c44c3f5e6fc77ecc98f34202b0d109881cfd
Electrolytful <erhanulusal@hotmail.com>
2023-10-05 15:52:21 +0100

    added comments, finalised project

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,6 +1,8 @@
+// getting the navigation links and the navigation button DOM elements
 const nav_items = document.getElementsByClassName("navigation__item");
 const nav_checkbox = document.getElementById("navi-toggle");
 
+// adding a click event listener to each navigation link, which runs an anonymous function that simulates a click event on the navigation button closing the nav menu
 for (let item of nav_items) {
     item.addEventListener("click", () => {
         nav_checkbox.click();
